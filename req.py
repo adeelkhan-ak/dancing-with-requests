164f340c29c586dc7de7a631d39f74e8cc4b3a62
adeelkhan-ak <ak140175@gmail.com>
2020-06-24 15:02:06 -0700

    changes

--- req.py
+++ req.py
@@ -6,4 +6,7 @@ print(response.text)            # To print formatted JSON response
 r_json = response.json()
 
 print(r_json['title'])
+data = {'title':'Pyton Requests','body':'Requests are qwesome','userId':1} 
+response = requests.post('https://jsonplaceholder.typicode.com/posts', data, stream = True) 
+print(response.raw.read(30))  
  
