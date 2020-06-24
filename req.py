96f122ca8a38b580b96fa35acf47a1c967bf514c
adeelkhan-ak <ak140175@gmail.com>
2020-06-24 13:55:26 -0700

    extra

---
+++ req.py
@@ -0,0 +1,9 @@
+import requests
+url = 'https://jsonplaceholder.typicode.com/todos/1' 
+response = requests.get(url)        # To execute get request 
+print(response.status_code)     # To print http response code  
+print(response.text)            # To print formatted JSON response 
+r_json = response.json()
+
+print(r_json['title'])
+ 
